2656eb9b6a0ffcc28a6ce9867db4dd15655540ab
BuildTools <unconfigured@null.spigotmc.org>
2020-02-07 13:25:05 +0100

    made a very simple miner that is way more efficient

---
+++ simple_miner.lua
@@ -0,0 +1,66 @@
+require("mover")
+local rb = require("robot")
+
+local function check(upOrDown)
+    if upOrDown == "up" then
+	for i=12,15 do
+	    rb.select(i)
+	    if rb.compareUp() then
+		return true
+	    end
+	end
+    elseif upOrDown == "down" then
+	for i=12,15 do
+	    rb.select(i)
+	    if rb.compareDown() then
+		return true
+	    end
+	end
+    else
+	for i=12,15 do
+	    rb.select(i)
+	    if rb.compare() then
+		return true
+	    end
+	end
+    end
+    return false
+end
+
+local function roundCheck()
+    if check("up") then
+	rb.swingUp()
+    end
+
+    if check("down") then
+	rb.swingDown()
+    end
+    
+    rb.turnLeft()
+    if check("") then
+	rb.swing()
+    end
+
+    rb.turnAround()
+    if check("") then
+	rb.swing()
+    end
+    rb.turnLeft()
+end
+
+
+local function forwardStep(overrideAmount)
+    if check("") or overrideAmount > 0 then
+	local goneF=goneForward + 1
+	roundCheck()
+	rb.swing()
+	rb.forward()
+	forwardStep(overrideAmount - 1)
+    else
+	roundCheck()
+	move(goneForward.."b")
+	rb.select(1)
+	rb.place()
+	goneForward = 0
+    end
+end
